4c8d25a3fe0d6d116214f89ef016d2667aa109f7
Omotara Dapo <omotara.dapo@gmail.com>
2025-05-01 21:22:50 +0100

    project commit

---
+++ dapo.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+
+let student = "John";
+
+console.log(student);
+
+const myArr = [1, 2, true, false, "love", "hate", ["a", "b", "c"]];
+
+const myObj = {
+  key1: "fix",
+  key2: "fix",
+};
