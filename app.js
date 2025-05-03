d28f5889f7b3b3a62de5f1309548891b4e1f6cf4
Ejinkeonye Erozanachukwu Shedrack <ejinmagnito@gmail.com>
2025-05-03 07:32:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const class = "fix";
+console.log("Hello");
+
+let myclass = "John"; // 'class is a reserved word
+
+console.log(myclass);
+
+const myArr = [1, 2, true, false, "love", "hate", ["a", "b", "c"]];
+
+const myObj = {
+  key1: "fix",
+  key2: "fix",
+};
+
+let integerNumber = 42; //Integer
+let decimalNumber = 3.14; //Decimal (floating-point)
+let negativeNumber = -7; //Negative number
